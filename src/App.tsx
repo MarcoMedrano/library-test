37f0644b4962d4a0cd02a9d680a467d84849f3e0
Marco Medrano <marco.medrano.garay@gmail.com>
2019-01-12 19:48:07 +0000

    Implemented basic main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,61 @@
 import * as React from 'react';
-import './App.css';
 
-import logo from './logo.svg';
+import { createStyles, Theme, withStyles, WithStyles } from '@material-ui/core/styles';
 
-class App extends React.Component {
+import AppBar from '@material-ui/core/AppBar';
+import IconButton from '@material-ui/core/IconButton';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import MenuIcon from '@material-ui/icons/Menu';
+// import MenuItem from '@material-ui/core/MenuItem';
+// import Menu from '@material-ui/core/Menu';
+// import Switch from '@material-ui/core/Switch';
+// import FormControlLabel from '@material-ui/core/FormControlLabel';
+// import FormGroup from '@material-ui/core/FormGroup';
+
+const styles = ({ spacing, palette }: Theme) => {
+  return createStyles({
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+  root: {
+    flexGrow: 1,
+  },
+});
+}
+const initialState = { currentPageIndex:0, pages:[] };
+type AppState = Readonly<typeof initialState>
+
+interface IAppProps extends WithStyles<typeof styles> {
+
+}
+
+class App extends React.Component<IAppProps, AppState> {
+
+
   public render() {
+    const { classes } = this.props;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" color="inherit" className={classes.grow}>
+              Library
+            </Typography>
+            
+          </Toolbar>
+        </AppBar>
       </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
